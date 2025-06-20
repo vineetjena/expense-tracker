3bf14f7c5c2ff818e34a006fe4e654165553fba7
vineetjena <136954835+vineetjena@users.noreply.github.com>
2025-06-21 00:27:36 +0530

    Update ExpenseForm.js

--- frontend/src/Components/Expenses/ExpenseForm.js
+++ frontend/src/Components/Expenses/ExpenseForm.js
@@ -81,7 +81,7 @@ function ExpenseForm() {
                 </select>
             </div>
             <div className="input-control">
-                <textarea name="description" value={description} placeholder='Add A Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
+                <textarea name="description" value={description} placeholder='Add A Description' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
             </div>
             <div className="submit-btn">
                 <Button 
