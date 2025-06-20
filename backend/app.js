230ac66a4109f2dfc3c0a727c855a6e47cec1e9d
vineetjena <136954835+vineetjena@users.noreply.github.com>
2025-06-20 23:57:41 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
-const {readdirSync} = require('fs')
+const { readdirSync } = require('fs')
 const app = express()
 
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000  
 
-//middlewares
+// middlewares
 app.use(express.json())
 app.use(cors())
 
-//routes
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
+
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
     db()
-    const PORT = 5000;
     app.listen(PORT, () => {
         console.log('listening to port:', PORT)
     })
